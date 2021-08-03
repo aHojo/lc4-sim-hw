d561c5e41f5cb587c0cd3407e59d4dc0d27371c0
ahojo <ahojodev@gmail.com>
2021-08-02 20:57:11 -0400

    removed changes from the header file

--- loader.c
+++ loader.c
@@ -7,6 +7,12 @@
 // memory array location
 unsigned short memoryAddress;
 
+// Function signatures
+int parse_header(unsigned short* address, unsigned short *length, unsigned short *line, unsigned short *index, FILE *file); // function to get address and number of lines.
+void load_memory(unsigned short address, unsigned short length, MachineState *machine ,FILE *file); // load memory into machine state.
+unsigned short int swap_two_bytes(unsigned short int num); // Swap the endianness
+int read_byte(unsigned short length, FILE *file); // read a byte from the file passed in.
+
 /*
  * Read an object file and modify the machine state as described in the writeup
  */
--- loader.h
+++ loader.h
@@ -7,7 +7,3 @@
 
 // Read an object file and modify the machine state as described in the writeup
 int ReadObjectFile(char* filename, MachineState* CPU);
-int parse_header(unsigned short* address, unsigned short *length, unsigned short *line, unsigned short *index, FILE *file); // function to get address and number of lines.
-void load_memory(unsigned short address, unsigned short length, MachineState *machine ,FILE *file); // load memory into machine state.
-unsigned short int swap_two_bytes(unsigned short int num); // Swap the endianness
-int read_byte(unsigned short length, FILE *file); // read a byte from the file passed in.
